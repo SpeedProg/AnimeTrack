bdc31ad3bdd68cea4cb12e41c781d8d01e8e0ffd
SpeedProg <constantin.wenger@googlemail.com>
2014-09-19 00:18:47 +0200

    hopefully fixed batoto handler to use new site bato.to :/

--- AnimeTrack/src/de/speedprog/animetrack/sitehandlers/batotonet/BatotoNetSiteHandler.java
+++ AnimeTrack/src/de/speedprog/animetrack/sitehandlers/batotonet/BatotoNetSiteHandler.java
@@ -8,48 +8,60 @@ import java.util.regex.Pattern;
 import de.speedprog.animetrack.sitehandlers.SiteHandler;
 
 public class BatotoNetSiteHandler implements SiteHandler {
-	private static final String REX_CHAPTERS = "<tr class=\"row lang_English chapter_row\">\\s+<td style=\"border-top:0;\">\\s+<a href=\"([\\w\\W&&[^\"]]+)\"><img src=\"http://www.batoto.net/book_open.png\" style=\"vertical-align:middle;\"/> (?:Vol.\\d+ ){0,1}Ch.(\\d+(?:\\.\\d+){0,1}).*</a>";
-	private static final Pattern PATTERN_CHAPTERS = Pattern
-			.compile(REX_CHAPTERS);
-	@Override
-	public boolean isHandled(URL url) {
-		if (url.getHost().equals("www.batoto.net")) {
-			return true;
-		}
-		return false;
-	}
+    public static void main(final String[] args) {
+        final String testUrl1 = "http://bato.to/comic/_/comics/tokyo-ghoul-r3056";
+        URL url = null;
+        try {
+            url = new URL(testUrl1);
+        } catch (final MalformedURLException e1) {
+            // TODO Auto-generated catch block
+            e1.printStackTrace();
+        }
+        final BatotoNetSiteHandler handler = new BatotoNetSiteHandler();
+        if (handler.isHandled(url)) {
+            try {
+                final int last = handler.getLastEpisodeFor(testUrl1, null);
+                System.out.println("Last Episode was: " + last);
+            } catch (final MalformedURLException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        } else {
+            System.err.println("URL not handled!");
+        }
+    }
 
-	@Override
-	public int getLastEpisodeFor(String url, String regEx)
-			throws MalformedURLException {
-		int lastChap = 0;
-		URL siteUrl = new URL(url);
-		String webSource = tk.speedprog.utils.WebContentUtils.getContentOfUrl(siteUrl);
-		Matcher chaptersMatcher = PATTERN_CHAPTERS.matcher(webSource);
-		while (chaptersMatcher.find()) {
-			String id = chaptersMatcher.group(2);
-			try {
-				int thisChap = Integer.parseInt(id);
-				if (thisChap > lastChap) {
-					lastChap = thisChap;
-				}
-			} catch (NumberFormatException e) {
-				System.err.println("Couldn't parse chap id "+id);
-			}
-		}
-		return lastChap;
-	}
-	
-	public static void main(String[] args) {
-		BatotoNetSiteHandler handler = new BatotoNetSiteHandler();
-		String testUrl1 = "http://www.batoto.net/comic/_/comics/dakara-boku-wa-h-ga-dekinai-r482";
-		try {
-			int last = handler.getLastEpisodeFor(testUrl1, null);
-			System.out.println("Last Episode was: "+last);
-		} catch (MalformedURLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
+    private static final String REX_CHAPTERS = "<tr class=\"row lang_English chapter_row\">\\s+<td style=\"border-top:0;\">\\s+<a href=\"([\\w\\W&&[^\"]]+)\"><img src=\"http://bato.to/book_open.png\" style=\"vertical-align:middle;\"/> (?:Vol.\\d+ ){0,1}Ch.(\\d+(?:\\.\\d+){0,1}).*</a>";
+    private static final Pattern PATTERN_CHAPTERS = Pattern
+            .compile(REX_CHAPTERS);
 
+    @Override
+    public int getLastEpisodeFor(final String url, final String regEx)
+            throws MalformedURLException {
+        int lastChap = 0;
+        final URL siteUrl = new URL(url);
+        final String webSource = tk.speedprog.utils.WebContentUtils
+                .getContentOfUrl(siteUrl);
+        final Matcher chaptersMatcher = PATTERN_CHAPTERS.matcher(webSource);
+        while (chaptersMatcher.find()) {
+            final String id = chaptersMatcher.group(2);
+            try {
+                final int thisChap = Integer.parseInt(id);
+                if (thisChap > lastChap) {
+                    lastChap = thisChap;
+                }
+            } catch (final NumberFormatException e) {
+                System.err.println("Couldn't parse chap id " + id);
+            }
+        }
+        return lastChap;
+    }
+
+    @Override
+    public boolean isHandled(final URL url) {
+        if (url.getHost().equals("bato.to")) {
+            return true;
+        }
+        return false;
+    }
 }
